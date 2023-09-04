823e36d99bb49ba8333e06e0ed370a69882e6367
yasmeen2001234 <83319374+yasmeen2001234@users.noreply.github.com>
2023-09-04 13:40:38 +0400

    weapon raycast - part1

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+
+    [SerializeField] float range = 100f;
+
+
+    [SerializeField] GameObject hitEffect;
+ 
+    [SerializeField] float FireRate = 0f;
+
+ 
+    [SerializeField] AudioSource audioSource;
+
+
+    public List<AudioClip> audioClipsList = new List<AudioClip>();
+  
+
+    bool canShoot = true;
+
+    
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && canShoot )
+        {
+            StartCoroutine(Shoot());
+         
+        }
+
+      
+    }
+
+
+    private IEnumerator Shoot()
+    {
+              canShoot = false;
+
+             ShootingRaycast();
+        
+            PlayAudioClipAtIndex(0); // play gunshot sound
+        
+        yield return new WaitForSeconds(FireRate); // wait for time to enable shooting
+        canShoot = true;
+    }
+
+
+
+
+    private void ShootingRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            HitImpact(hit);
+        }
+    }
+
+    // creating a visual impact effect at the point where a raycast hit an object
+    private void HitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
+
+    }
+
+    private void PlayAudioClipAtIndex(int index)
+    {
+        if (index >= 0 && index < audioClipsList.Count)
+        {
+            audioSource.PlayOneShot(audioClipsList[index]);
+        }
+    }
+
+
+
+}
